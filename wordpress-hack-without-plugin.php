6c474f43615410d5f626421ea53ef52a38dd15d4
namdi <nnamsok@gmail.com>
2017-10-28 10:05:21 +0100

    wplogo-urlupdate

--- wordpress-hack-without-plugin.php
+++ wordpress-hack-without-plugin.php
@@ -82,7 +82,7 @@ return get_bloginfo( 'url' );
 add_filter( 'login_headerurl', 'my_login_logo_url' );
 
 function my_login_logo_url_title() {
-return 'Vendes Nigeria';
+return 'add_your_text/value here';
 }
 add_filter( 'login_headertitle', 'my_login_logo_url_title' );
 
